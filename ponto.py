e2d7e12c376379aa9fa9e9d3fa973b3600010f65
sandrim01 <aless0791naval@gmail.com>
2025-07-31 14:54:47 -0300

    Implementa rota e função bater_ponto no controle de ponto e corrige template

--- ponto.py
+++ ponto.py
@@ -2,6 +2,24 @@ from flask import Blueprint, render_template
 
 bp_ponto = Blueprint('ponto', __name__, url_prefix='/ponto')
 
-@bp_ponto.route('/')
+from flask import request, redirect, url_for, flash
+from models import Ponto, db
+from flask_login import login_required, current_user
+import datetime
+
+@bp_ponto.route('/', methods=['GET'])
+@login_required
 def ponto_index():
-    return render_template('employees/ponto.html')
+    registros = Ponto.query.filter_by(user_id=current_user.id).order_by(Ponto.data.desc(), Ponto.hora_entrada.desc()).all()
+    return render_template('employees/ponto.html', registros=registros)
+
+@bp_ponto.route('/bater', methods=['POST'])
+@login_required
+def bater_ponto():
+    # Aqui você pode adicionar lógica para salvar a foto, validar horário, etc.
+    agora = datetime.datetime.now()
+    novo_ponto = Ponto(user_id=current_user.id, data=agora.date(), hora_entrada=agora)
+    db.session.add(novo_ponto)
+    db.session.commit()
+    flash('Ponto registrado com sucesso!', 'success')
+    return redirect(url_for('ponto.ponto_index'))
